e00c4d846d8df7399db05ba9f0e381f7c65b785e
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-17 10:09:00 -0300

    component: project: setup project
    Configure ESLint and Prettier
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from './components/ui/button'
 import './global.css'
 
 export function App() {
 
   return (
-    <h1>Pizza Shop</h1>
+    <div className="flex justify-center items-center h-screen">
+        <Button>Enviar</Button>
+    </div>
   )
 }
-
