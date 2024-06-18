e18eb19fb1c377b33a3006e4a99117d994646cca
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-18 11:02:36 -0300

    component: pages: add notification
    Install and configure notification `toast(sonner)`
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './pages/routes'
 
@@ -9,7 +10,7 @@ export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | pizza.shop" />
-
+      <Toaster richColors closeButton />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
