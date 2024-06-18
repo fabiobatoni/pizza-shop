423a56497fce554667344c49600a92d5f13fa299
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-18 14:48:45 -0300

    component: pages: dashboard: create page
    Create page Dashboard with cards
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
-import { Dashboard } from './app/dashboard'
+import { Dashboard } from './app/dashboard/dashboard'
 import { Orders } from './app/orders/orders'
 import { SignIn } from './auth/sign-in'
 import { SignUp } from './auth/sign-up'
