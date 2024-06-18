b70615ae07430deee8c2d70fbdbbebe0e1c9755d
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-18 15:30:48 -0300

    component: page: 404: create 404
    Create page Not Found
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
+import { NotFound } from './404'
 import { Dashboard } from './app/dashboard/dashboard'
 import { Orders } from './app/orders/orders'
 import { SignIn } from './auth/sign-in'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
