55ec15f306c953c5f45216f94765172577fc229b
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-18 13:37:19 -0300

    components: pages: orders: create table
    Create table with the shadcn/ui table
    component
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './_layouts/app'
 import { AuthLayout } from './_layouts/auth'
 import { Dashboard } from './app/dashboard'
+import { Orders } from './app/orders/orders'
 import { SignIn } from './auth/sign-in'
 import { SignUp } from './auth/sign-up'
 
@@ -10,7 +11,10 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    children: [{ path: '/', element: <Dashboard /> }],
+    children: [
+      { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <Orders /> },
+    ],
   },
   {
     path: '/',
